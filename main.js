3d345f525c3109119e0d408c33c371e08e20a95c
AarushAarush <106853995+AarushAarush@users.noreply.github.com>
2023-01-03 18:46:21 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
+noseX = 0;
+noseY = 0;
 function preload(){
-
+clownnose = loadImage('https://i.postimg.cc/cHvL1kSJ/clown-nose-clipart-2.png');
 }
 function setup(){
 canvas = createCanvas(450, 400);
 canvas.center()
 video = createCapture(VIDEO);
-video.size(300, 300);
+video.size(450, 400);
 video.hide();
 poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 function draw(){
 image(video, 0, 0, 450, 400);
+//fill(255,0,0);
+//stroke(255,0,0);
+//circle(noseX, noseY, 20);
+image(clownnose, noseX, noseY, 30, 30);
 }
 function Snap(){
 save('ClownMe.png');
@@ -23,6 +29,8 @@ function gotPoses(results){
 if(results.length > 0)
 {
 console.log(results);
+noseX = results[0].pose.nose.x - 20;
+noseY = results[0].pose.nose.y - 20;
 console.log("nose x = " + results[0].pose.nose.x);
 console.log("nose y = " + results[0].pose.nose.y);
 }
